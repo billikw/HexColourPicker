63bb824aa8715e6c7ebb3d420684bdce9e159648
billikw <66752314+billikw@users.noreply.github.com>
2020-06-10 23:22:06 +0100

    Add files via upload

--- hexscript.js
+++ hexscript.js
@@ -1,14 +1,19 @@
 const hexArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 function changeColour() {
-  let hex = "#";
-  for (i = 0; i < 6; i++) {
-    hex += hexArray[randomPosition()];
-  }
-  console.log(hex);
+  hex = getHex();
   document.getElementById("background").style.backgroundColor = hex;
   document.getElementById("hexText").innerHTML = hex;
 }
-function randomPosition() {
-  return Math.floor(Math.random() * hexArray.length);
+
+function getHex() {
+  let hex = "#";
+  for (i = 0; i < 6; i++) {
+    hex += hexArray[randomPosition(hexArray)];
+  }
+  return hex;
+}
+
+function randomPosition(array) {
+  return Math.floor(Math.random() * array.length);
 }
